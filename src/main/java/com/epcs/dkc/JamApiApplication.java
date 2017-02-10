299d685045a54f2b5151dad3e7262d05b6d7ebff
li2137 <@purdue.edu>
2017-02-10 14:30:05 -0500

    Initial Commit with Spring Boot

---
+++ src/main/java/com/epcs/dkc/JamApiApplication.java
@@ -0,0 +1,12 @@
+package com.epcs.dkc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JamApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JamApiApplication.class, args);
+	}
+}
---
+++ src/test/java/com/epcs/dkc/JamApiApplicationTests.java
@@ -0,0 +1,16 @@
+package com.epcs.dkc;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JamApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
