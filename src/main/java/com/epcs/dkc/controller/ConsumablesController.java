e6377e7efed269f8845137e05554492ed43f244d
potatoshane <li2137@purdue.edu>
2017-03-31 01:03:11 -0400

    Change update from GET to POST

--- src/main/java/com/epcs/dkc/controller/ConsumablesController.java
+++ src/main/java/com/epcs/dkc/controller/ConsumablesController.java
@@ -4,10 +4,7 @@ import com.epcs.dkc.data.Communicator;
 import com.epcs.dkc.data.Consumables;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by shane on 3/31/17.
@@ -36,15 +33,7 @@ public class ConsumablesController {
     }
 
     @GetMapping(path="/update")
-    public @ResponseBody String update(@RequestParam int id, @RequestParam String name, @RequestParam String description,
-                                     @RequestParam double price, @RequestParam int modifier_id, @RequestParam int size_id) {
-        Consumables consumable = new Consumables();
-        consumable.setName(name);
-        consumable.setDescription(description);
-        consumable.setPrice(price);
-        consumable.setModifier_id(modifier_id);
-        consumable.setSize_id(size_id);
-
+    public @ResponseBody String update(@ModelAttribute Consumables consumable) {
         return dataCommunicator.updateConsumable(consumable);
     }
 }
