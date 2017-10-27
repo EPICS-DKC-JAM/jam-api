685c24e6604737c5f29019d9c5b86d4888d441b3
potatoshane <li2137@purdue.edu>
2017-10-27 14:00:32 -0400

    Fixed port issue

--- app.js
+++ app.js
@@ -35,6 +35,12 @@ app.use('/modifiers', modifiers);
 app.use('/sizes', sizes);
 
 app.set('port', (process.env.PORT || 3000));
+app.get('/', function(request, response) {
+  var result = 'App is running'
+  response.send(result);
+}).listen(app.get('port'), function() {
+  console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 // Connect to Mongo
 var mongoUrl = config.mongoUrl;
